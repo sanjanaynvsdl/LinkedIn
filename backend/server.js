ea980823116dcb5d958d442d6837ff234aa3dc30
sanjanaynvsdl <sanjanayalamarthi@gmail.com>
2024-12-19 18:28:26 +0530

    Production ready

--- backend/server.js
+++ backend/server.js
@@ -8,19 +8,25 @@ import connectionRoutes from './routes/connection.route.js'
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './lib/db.js';
+import path from 'path';
 
 
 
 
 dotenv.config();
 const app=express();
+const __dirname=path.resolve();
 
-app.use(
-    cors({
-    origin:"http://localhost:5173",
-    credentials:true,                         //allowing them to send cookies with the response,
-})
-);
+
+if(process.env.NODE_ENV !=="production") {
+    app.use(
+        cors({
+            origin:"http://localhost:5173",
+            credentials:true,                         //allowing them to send cookies with the response,
+        })
+    );
+}
+
 //Added limit for the images (else- throws an error- payload too large)
 // { limit : "5mb "} (removed from below express.json())
 app.use(express.json()); //middleware -> Parse JSON request bodies to js object,
@@ -36,12 +42,19 @@ app.use("/api/v1/connections",connectionRoutes);
 
 
 
-app.get('/', (req, res)=> {
-    return res.json({
-        message:"Hello"
-    });
-});
+// app.get('/', (req, res)=> {
+//     return res.json({
+//         message:"Hello"
+//     });
+// });
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
